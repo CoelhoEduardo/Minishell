992782021f077f4f3f776367903660fd9d0263d9
Eduardo Coelho <educs98@outlook.com>
2024-06-19 12:44:29 -0300

    shazam ctrl-c final version

--- include/mini/contexts/signals.h
+++ include/mini/contexts/signals.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eduardocoelho <eduardocoelho@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 18:59:50 by bhildebr          #+#    #+#             */
-/*   Updated: 2024/06/15 18:31:51 by bhildebr         ###   ########.fr       */
+/*   Updated: 2024/06/19 12:40:48 by eduardocoel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ struct s_signals_context {
 };
 
 t_none	mini_signals_create(t_mini mini);
-t_none	mini_signals_handler(int sig, siginfo_t *info, void *context);
-t_none	mini_signals_sigint_handler(t_mini mini, t_i32 sig);
-t_none	mini_signals_sigquit_handler(t_mini mini);
+t_none	mini_signals_sigint_handler(t_i32 sig);
 
 #endif
--- src/mini/contexts/signals/mini_signals_create.c
+++ src/mini/contexts/signals/mini_signals_create.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mini_signals_create.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eduardocoelho <eduardocoelho@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:01:47 by bhildebr          #+#    #+#             */
-/*   Updated: 2024/06/17 13:56:36 by bhildebr         ###   ########.fr       */
+/*   Updated: 2024/06/19 12:43:10 by eduardocoel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ t_none	mini_signals_create(t_mini mini)
 	mini->signals = mini_alloc(mini, sizeof(struct s_signals_context));
 	mini->signals->sigaction = mini_alloc(mini, sizeof(struct sigaction));
 	mem_set(mini->signals->sigaction, 0, sizeof(struct sigaction));
-	mini->signals->sigaction->sa_sigaction = mini_signals_handler;
-	mini->signals->sigaction->sa_flags = SA_SIGINFO;
-	mini_assert(mini, sigaction(SIGINT, mini->signals->sigaction, NULL) != -1, MINI_ERROR);
-	mini_assert(mini, sigaction(SIGQUIT, mini->signals->sigaction, NULL) != -1, MINI_ERROR);
+	mini->signals->sigaction->sa_handler = mini_signals_sigint_handler;
+	sigemptyset(&mini->signals->sigaction->sa_mask);
+	mini->signals->sigaction->sa_flags = 0;
+	sigaction(SIGINT, mini->signals->sigaction, NULL);
 	signal(SIGQUIT, SIG_IGN);
 }
