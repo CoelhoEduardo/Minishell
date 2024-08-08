d0bc4d412a3d72a3a14df7e84c921bf2dc94b309
Eduardo Coelho <educs98@outlook.com>
2024-08-07 22:35:07 -0300

    fine tunes

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 16:33:49 by bhildebr          #+#    #+#             */
-/*   Updated: 2024/07/22 21:58:31 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2024/08/07 21:16:07 by ecoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ int	execute_env(t_token *cmd)
 
 	env = *get_my_env();
 	if (cmd->next)
-		return (!!write(STDERR_FILENO, "env: too many arguments", 23));
-	i = -1;
-	while (env[++i])
+		return (!!write(STDERR_FILENO, "env: too many arguments\n", 24));
+	i = 0;
+	while (env[i])
 	{
-		if (env[i][0] && ft_strchr(env[i], '='))
-			ft_fprintf(STDOUT_FILENO, "%s\n", env[i]);
+		ft_putstr_fd(env[i], STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		i++;
 	}
 	return (SUCCESS);
 }
--- src/utils/line_prompt.c
+++ src/utils/line_prompt.c
@@ -6,7 +6,7 @@
 /*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 18:48:50 by ecoelho-          #+#    #+#             */
-/*   Updated: 2024/08/02 18:54:51 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2024/08/07 20:48:22 by ecoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,20 +39,24 @@ char	*ft_strcpy(char *dest, const char *src)
         dest[i] = src[i];
         i++;
     }
+	dest[i] = '\0';
 	return (dest);
 }
 
-char	*cut_pwd(const char *path)
+char	*cut_pwd(char *path)
 {
 	const char	*first_slash;
 	const char	*second_slash;
+	char		*new_pwd;
 
 	first_slash = ft_strdup(ft_strchr(path, '/') + 1);
 	second_slash = ft_strchr(first_slash, '/');
 	if (second_slash == NULL)
-		return (ft_strdup(path));
+		new_pwd = ft_strdup(path);
 	else
-		return (ft_strdup(second_slash + 1));
+		new_pwd = ft_strdup(second_slash + 1);
+	free(path);
+	return (new_pwd);
 }
 
 char	*prompt_line(void)
@@ -61,20 +65,19 @@ char	*prompt_line(void)
 	char	*end_line;
 	char	*final;
 	char	*name;
-	char	*just_path;
 	size_t	total_len;
 
 	name = "minishell:~/";
 	end_line = "⚡️ ";
-	cwd = getcwd(NULL, 0);
-	just_path = cut_pwd(cwd);
-	free(cwd);
-	total_len = ft_strlen(name) + ft_strlen(just_path) + strlen(end_line) + 1;
+	cwd = cut_pwd(getcwd(NULL, 0));
+	total_len = ft_strlen(name) + ft_strlen(cwd) + strlen(end_line) + 1;
 	final = malloc(total_len);
-	if (final == NULL)
+	if (!final)
 		return (NULL);
 	ft_strcpy(final, name);
-	ft_strcat(final, just_path);
+	ft_strcat(final, cwd);
 	ft_strcat(final, end_line);
+	// free(cwd);
+	ft_collect_mem(final);
 	return (final);
 }
